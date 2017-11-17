8e5034db214c853a5c46c80de8bbc1e943e1650f
SkyLightSK <kopel@aport.ru>
2017-11-17 13:41:42 +0200

    front-end 1.3v

---
+++ app/js/modules/popup.js
@@ -0,0 +1,20 @@
+$(function() {
+
+    // contact form animations
+    $('.footer_button').click(function() {
+        $('#contactForm').fadeToggle();
+        $('.cover').fadeToggle();
+    })
+    $(document).mouseup(function (e) {
+        var container = $("#contactForm");
+        var cover = $('.cover');
+
+        if (!container.is(e.target) // if the target of the click isn't the container...
+            && container.has(e.target).length === 0) // ... nor a descendant of the container
+        {
+            container.fadeOut();
+            cover.fadeOut();
+        }
+    });
+
+});
